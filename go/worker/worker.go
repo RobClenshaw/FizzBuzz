bc662d644020f4b7a30b13740b5ade3c77edf050
Rob Clenshaw <robert.clenshaw@red-gate.com>
2021-05-06 17:42:21 +0100

    Extract functions

--- go/worker/worker.go
+++ go/worker/worker.go
@@ -9,43 +9,44 @@ import (
 )
 
 var count = 0
+var hostName string = os.Getenv("HOSTNAME")
+var divisor string = os.Getenv("DIVISOR")
+var outputPhrase string = os.Getenv("OUTPUT_PHRASE")
 
 func main() {
-	hostName := os.Getenv("HOSTNAME")
-	divisor := os.Getenv("DIVISOR")
-	outputPhrase := os.Getenv("OUTPUT_PHRASE")
-
-	http.HandleFunc("/data/", func(w http.ResponseWriter, r *http.Request) {
-		input := r.URL.Path[6:len(r.URL.Path)]
-		number, err := strconv.Atoi(input)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			io.WriteString(w, "Expected a positive integer, but got "+input)
-			return
-		}
-
-		divisor, _ := strconv.Atoi(divisor)
-
-		payload := outputPayload{
-			OutputString: getOutputString(number, divisor, outputPhrase),
-			Host:         hostName,
-		}
-
-		output, _ := json.Marshal(payload)
-
-		io.WriteString(w, string(output))
-
-		count++
-	})
-
-	http.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
-		if count > 0 {
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-	})
-
+	http.HandleFunc("/data/", handleData)
+	http.HandleFunc("/ready", handleReady)
 	http.ListenAndServe(":80", nil)
+}
+
+func handleData(w http.ResponseWriter, r *http.Request) {
+	input := r.URL.Path[6:len(r.URL.Path)]
+	number, err := strconv.Atoi(input)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "Expected a positive integer, but got "+input)
+		return
+	}
+
+	divisor, _ := strconv.Atoi(divisor)
+
+	payload := outputPayload{
+		OutputString: getOutputString(number, divisor, outputPhrase),
+		Host:         hostName,
+	}
+
+	output, _ := json.Marshal(payload)
+
+	io.WriteString(w, string(output))
+
+	count++
+}
+
+func handleReady(w http.ResponseWriter, r *http.Request) {
+	if count > 0 {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
 func getOutputString(input int, divisor int, outputPhrase string) string {

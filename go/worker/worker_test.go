136417a1ff8080c34847c91bea2dad3a535b4b55
Rob Clenshaw <robert.clenshaw@red-gate.com>
2021-05-06 18:11:16 +0100

    Add test for ready returning 200 before any calls

--- go/worker/worker_test.go
+++ go/worker/worker_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -15,5 +17,24 @@ func TestReturnsEmptyStringWhenNotMultipleOfDivisor(t *testing.T) {
 	output := getOutputString(7, 3, "Fizz")
 	if output != "" {
 		t.Errorf("Expected empty string for 7 but got \"%s\"", output)
+	}
+}
+
+func TestReturnsReadyIfNoDataRequests(t *testing.T) {
+	req, err := http.NewRequest("GET", "/ready", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	count := 0
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handleReady(w, r, &count)
+	})
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Ready request returned wrong status. Expected %v but got %v", http.StatusOK, rr.Code)
 	}
 }
